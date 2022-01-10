78494652db880c3e6e11037bb12befe664bcb8c4
niculas1989 <nicolas.maranzano@libero.it>
2022-01-10 14:52:16 +0100

    added subdivision - comments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log('JS OK');
+
+// CONSEGNA
+
+/* 
+
+# Mail
+Crea un array di email;
+Chiedi all’utente la sua email.
+controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+
+
+# Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampare in console i tiri di dado e il risultato.
+
+
+#1 MAIl
+
+1 - creiamo un ARRAY in cui inseriamo un "tot" di email tra le quali dobbiamo cercare.
+2 - attraverso un prompt chiediamo all'utente di inserire la sua mail
+3 - SE la mail è nella nostra lista, l'utente può accedere e stampiamo un determinato messaggio - ALTRIMENTI ne stampiamo un altro.
+
+
+#2 DADI
+
+1 - creare un contatore che permetta di randomizzare i numeri da 1 a 6
+2 - farlo per due volte (il randomizzatore)
+3 - decreatare quale sia il numero più grande (primo tiro umano, secondo computer)
+4 - stampare il tutto in console
+
+5 - e se pareggiano?
