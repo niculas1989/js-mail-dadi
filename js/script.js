22bddcc1320a4fb421631af6e14025a7946cfbf6
niculas1989 <nicolas.maranzano@libero.it>
2022-01-10 16:02:20 +0100

    add second try for exercise mail

--- js/script.js
+++ js/script.js
@@ -17,16 +17,32 @@ controlla che sia nella lista di chi può accedere, stampa un messaggio appropri
 3 - SE la mail è nella nostra lista, l'utente può accedere e stampiamo un determinato messaggio - ALTRIMENTI ne stampiamo un altro.
 */
 
-const email = ['nicolas.maranzano@libero.it', '@gmail.com', '@yahoo.it', '@libero.it'];
+// const email = ['nicolas.maranzano@libero.it', '@gmail.com', '@yahoo.it', '@libero.it'];
 
-const yourMail = prompt('Scrivi di seguito la tua mail', 'nicolas.maranzano@libero.it');
+// const yourMail = prompt('Scrivi di seguito la tua mail', 'nicolas.maranzano@libero.it');
 
+
+// for (let i = 0; i < email.length; i++) {
+//     if (yourMail === 0) {
+//         alert('Il tuo login è andato a buon fine');
+//     }
+//     else {
+//         alert('Il nome utente o la password sono errate');
+//     }
+// }
+
+
+const email = ['nicolas.maranzano@libero.it', 'gmail.com', 'yahoo.it'];
+let yourMail = prompt("Scrivi di seguito la tua mail", 'nicolas.maranzano@libero.it');
 
 for (let i = 0; i < email.length; i++) {
-    if (yourMail === 0) {
-        alert('Il tuo login è andato a buon fine');
-    }
-    else {
-        alert('Il nome utente o la password sono errate');
+    switch (email) {
+        case 'nicolas.maranzano@libero.it':
+            alert("Login effettuata con successo");
+            break;
+        default:
+            alert("Nome utente o password non corrette");
     }
 }
+
+
